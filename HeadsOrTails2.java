6daca2e05fa35e26496d5e0b368d40e653b94c67
SKoutarou <Koutarou041119@icloud.com>
2024-07-04 23:17:07 +0900

    User name added

--- HeadsOrTails2.java
+++ HeadsOrTails2.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class HeadsOrTails {
+public class HeadsOrTails2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
